7e4ce8234d3b4dbc9f58512af9247386105cf969
Denovo <vo0v0000@naver.com>
2023-05-26 14:51:35 +0900

    7 - Readonly

--- challenge/easy/Pick.ts
+++ challenge/easy/Pick.ts
@@ -1,12 +1,14 @@
-interface Todo {
-    title: string;
-    description: string;
-    completed: boolean;
-}
-type MyPick<T, K extends keyof T> = { [k in K]: T[k] };
-type TodoPreview = MyPick<Todo, "title" | "completed">;
+namespace Pick {
+    interface Todo {
+        title: string;
+        description: string;
+        completed: boolean;
+    }
+    type MyPick<T, K extends keyof T> = { [k in K]: T[k] };
+    type TodoPreview = MyPick<Todo, "title" | "completed">;
 
-const todo: TodoPreview = {
-    title: "Clean room",
-    completed: false,
-};
+    const todo: TodoPreview = {
+        title: "Clean room",
+        completed: false,
+    };
+}
