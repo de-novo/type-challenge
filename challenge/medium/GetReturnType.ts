4262e12416c46593a1c49484099a1f33aedb6fd5
Denovo <vo0v0000@naver.com>
2023-05-26 23:36:02 +0900

    2 - Get Return Type

---
+++ challenge/medium/GetReturnType.ts
@@ -0,0 +1,12 @@
+namespace GetReturnType {
+    const fn = (v: boolean) => {
+        if (v) return 1;
+        else return 2;
+    };
+
+    type MyReturnType<T extends Function> = T extends (...arg: any) => infer R
+        ? R
+        : never;
+
+    type a = MyReturnType<typeof fn>; // should be "1 | 2"
+}
