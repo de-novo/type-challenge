f279eaed233d2fae1f321c05028375f20f1133d9
Denovo <vo0v0000@naver.com>
2023-05-27 00:05:48 +0900

    3 - Omit

---
+++ challenge/medium/Omit.ts
@@ -0,0 +1,25 @@
+namespace Omit {
+    interface Todo {
+        title: string;
+        description: string;
+        completed: boolean;
+    }
+    type Exclude<T, K> = T extends K ? never : T;
+    type MyOmit<T, K extends keyof T> = {
+        [k in Exclude<keyof T, K>]: T[k];
+    };
+
+    type MyOmit2<T, K extends keyof T> = {
+        [P in keyof T as P extends K ? never : P]: T[P];
+    };
+    type TodoPreview = MyOmit<Todo, "description" | "title">;
+    type TodoPreview2 = MyOmit2<Todo, "description" | "title">;
+    const todo: TodoPreview = {
+        completed: true,
+    };
+    const todo2: TodoPreview2 = {
+        completed: true,
+    };
+    type error = MyOmit<Todo, "description" | "invalid">;
+    type error2 = MyOmit2<Todo, "description" | "invalid">;
+}
