b84fcbeefe5df3968f97d9885aae22b1368e2bd6
Denovo <vo0v0000@naver.com>
2023-05-26 16:06:47 +0900

    268 - If

---
+++ challenge/easy/If.ts
@@ -0,0 +1,6 @@
+namespace If {
+    type If<C extends boolean, T, F> = C extends true ? T : F;
+    type A = If<true, "a", "b">; // expected to be 'a'
+    type B = If<false, "a", "b">; // expected to be 'b'
+    type Error = If<null, "a", "b">; // error 
+}
